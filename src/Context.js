b215bf8c2722e5d05e25c041d4913b971dfd78bf
yildiztugba <okra.yildiz@gmail.com>
2023-05-25 12:58:37 +0200

    context setup

---
+++ src/Context.js
@@ -0,0 +1,12 @@
+import React from "react"
+const Context = React.createContext()
+
+function ContextProvider(props){
+
+    return (
+        <Context.Provider value="">
+            {props.children}
+        </Context.Provider>
+    )
+}
+export  {ContextProvider,Context}
--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ContextProvider} from "./Context"
 import {BrowserRouter as Router} from "react-router-dom"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <ContextProvider>
+      <Router>
       <App />
     </Router>
+    </ContextProvider>
     
   </React.StrictMode>
 );
