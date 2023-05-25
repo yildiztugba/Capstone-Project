3225f17ad7456918c873c7ffa1a1d51e1d2c0cbf
yildiztugba <okra.yildiz@gmail.com>
2023-05-25 13:07:13 +0200

    fetch data to state

--- src/Context.js
+++ src/Context.js
@@ -1,10 +1,17 @@
-import React from "react"
+import React,{useState,useEffect} from "react"
 const Context = React.createContext()
 
 function ContextProvider(props){
 
+    const [photos, setPhotos] = useState([])
+    const url="https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
+    useEffect(()=>{
+        fetch(url).then(res=>res.json())
+        .then(data=>setPhotos(data))
+    },[])
+
     return (
-        <Context.Provider value="">
+        <Context.Provider value={{photos}}>
             {props.children}
         </Context.Provider>
     )
