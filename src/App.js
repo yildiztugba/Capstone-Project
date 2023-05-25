228bb85a7b694bf11ad9ef30412e654f1418a5c9
yildiztugba <okra.yildiz@gmail.com>
2023-05-25 10:19:09 +0200

    setup react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import React from "react"
 import Header from "./components/Header"
-import Cart from "./pages/Cart"
 import Photos from "./pages/Photos"
+import {Routes,Route} from "react-router-dom"
+import Cart from "./pages/Cart"
 function App() {
   return (
-     <div>
+    <div>
             <Header />
             <h1>Home Page</h1>
+            <Routes>
+            <Route path="/" element={<Photos />} />
+            <Route path="/cart" element={<Cart />} />
+            </Routes>
         </div>
   );
 }
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,8 +1,10 @@
 import React from "react"
 
 function Cart() {
+
     return (
         <main className="cart-page">
+          
             <h1>Check out</h1>
         </main>
     )
