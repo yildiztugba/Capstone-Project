fbf97274a613cbf5bf7dbe59a80d1886d542e592
yildiztugba <okra.yildiz@gmail.com>
2023-05-31 23:46:42 +0200

    updated header cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
-import React from "react"
+import React , {useContext} from "react"
+import {Context} from "../Context"
 import {Link} from "react-router-dom"
 function Header() {
+    const context = useContext(Context)
+    const cartClassName = context.cartItems.length > 0 ? "ri-shopping-cart-fill" : "ri-shopping-cart-line"
+    
     return (
         <header>
         <Link to="/"> <h2>Pic Some</h2></Link>
-        <Link to="cart">  <i className="ri-shopping-cart-line ri-fw ri-2x"></i></Link>
+        <Link to="/cart">  <i className={`${cartClassName} ri-fw ri-2x`}></i></Link>
         </header>
     )
 }
